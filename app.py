2edcbe6ff738f663b1f0c11b05fb17d08a9dc260
Chandrabhanu Prusty <prustycb18@gmail.com>
2026-09-18 11:40:53 +0530

    Add Python calculator application

---
+++ app.py
@@ -0,0 +1,14 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+if __name__ == "__main__":
+    print("DevOps Python Application")
+    print("Addition: 10 + 5 =", add(10, 5))
+    print("Subtraction: 10 - 5 =", subtract(10, 5))
+    print("Multiplication: 10 * 5 =", multiply(10, 5))
